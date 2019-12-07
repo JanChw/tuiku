f722784716e06b201c8e59345c83df307bc83bce
Jan's Mac <1134806787@qq.com>
2019-12-07 19:26:13 +0800

    连接数据库并创建post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { PostModule } from './modules/post/post.module'
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(),PostModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
